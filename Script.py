fe9db2b2e8ce0570416b74ca134eda5bfa9ecc5c
Jibesh Patra <jibesh.patra@gmail.com>
2015-07-30 09:47:03 +0200

    Some code cleanup

--- Script.py
+++ Script.py
@@ -66,97 +66,79 @@ def indent(elem, level=0):
 
 
 def checkData(csvfile, hos_name):
-    p=[]
+    p = []
     for row in csvfile:
-        if row[0] == "#":
-            continue
         try:
             int(row[1])
             int(row[2])
             int(row[3])
         except ValueError:
-            logger.info("Bed count not in integer values, hosp name : "+hos_name+" bed_type : "+row[0])
-            return 0
+            logger.info("Bed count not in integer values, hosp name : " + hos_name + " bed_type : " + row[0])
+            return False
         for r in row:
             if r == '':
-                logger.info("File does not contain whole data, bed_type = "+row[0])
-                return 0
+                logger.info("File does not contain whole data, bed_type = " + row[0])
+                return False
         if int(row[1]) < (int(row[2]) + int(row[3])):
             logger.info("Total beds less than Reserved and Occupied")
-            return 0
+            return False
         if int(row[1]) < 0 or int(row[2]) < 0 or int(row[3]) < 0:
             logger.info("Bed count is negative")
-            return 0
+            return False
         if row[0].upper() not in hospitals[hos_name]:
             logger.info("This type of bed doesnot exist for " + hos_name)
-            return 0
+            return False
         p.append(row[0])
-    b=list(set(p))
+    b = list(set(p))
     p.sort()
     b.sort()
     if p != b:
         logger.info("Duplicates in bed_type")
-        return 0
-    return 1
+        return False
+    return True
 
 
 def reader_writer_sender():
-    flag = 0
     bed_info = ['Total', 'Reserved', 'Occupied', 'Available']
     for filename in glob.glob('*.csv'):
         try:
             with open(filename, 'rb') as f:
-                hos_name = "null"
-                flag = 0
-                flag1 = 0
                 csvfile = csv.reader(f)
                 dataValueSet = ET.Element("dataValueSet")
                 dataValueSet.set("xmlns", "http://dhis2.org/schema/dxf/2.0")
-                for key in hospitals:
-                    if key.lower() in filename.lower():
-                        hos_name = key
-                        dataValueSet.set("dataSet", hospitals[hos_name]['hosp_dets']['dataSet'])
-                        dataValueSet.set("orgUnit", hospitals[hos_name]['hosp_dets']['orgUnit'])
-                        logger.info("Hospital Name : " + hos_name)
-                        flag1 = 1
-                        break
-                if flag1 == 0:
-                    logger.info("The filename sent by hospital is not valid : "+filename[:-4])
+                if filename.lower()[:-4] in hospitals:
+                    hos_name = filename.lower()[:-4]
+                    print ("Processing " + hos_name)
+                    dataValueSet.set("dataSet", hospitals[hos_name]['hosp_dets']['dataSet'])
+                    dataValueSet.set("orgUnit", hospitals[hos_name]['hosp_dets']['orgUnit'])
+                    with open(filename, 'rb') as fl:
+                        if (checkData(csv.reader(fl), hos_name)):
+                            for row in csvfile:
+                                index = 1
+                                bed_type = row[0].upper()
+                                dataValueSet.set("completeDate", row[4])
+                                dataValueSet.set("period", row[4][:8])
+                                for b in bed_info:
+                                    dataValue = ET.SubElement(dataValueSet, "dataValue")
+                                    dataValue.set("dataElement", hospitals[hos_name][bed_type][b])
+                                    if index == 4:
+                                        dataValue.set("value", str(int(row[1]) - int(row[2]) - int(row[3])))
+                                        break
+                                    else:
+                                        dataValue.set("value", row[index])
+                                    index += 1
+                            indent(dataValueSet)
+                            tree = ET.ElementTree(dataValueSet)
+                            xml_file_name = filename[:-4]
+                            xml_file = xml_file_name + ".xml"
+                            tree.write(xml_file, xml_declaration=True, encoding='utf-8', method="xml")
+                            # os.system("curl -d @" +xml_file+ " ""http://180.149.243.107:8080/api/dataValueSets"" -H ""Content-Type:application/xml"" -u admin:district -v")
+                else:
+                    logger.info("The filename sent by hospital is not valid : " + filename)
                     continue
-                with open(filename, 'rb') as fl:
-                    csvf = csv.reader(fl)
-                    flag1 = checkData(csvf, hos_name)
-                if flag1 == 0:
-                    continue
-                for row in csvfile:
-                    index = 1
-                    if row[0] in "#":
-                        flag = 1
-                        continue
-                    bed_type = row[0].upper()
-                    dataValueSet.set("completeDate", row[4])
-                    dataValueSet.set("period", row[4][:8])
-                    for b in bed_info:
-                        dataValue = ET.SubElement(dataValueSet, "dataValue")
-                        dataValue.set("dataElement", hospitals[hos_name][bed_type][b])
-                        if index == 4:
-                            dataValue.set("value", str(int(row[1]) - int(row[2]) - int(row[3])))
-                            break
-                        else:
-                            dataValue.set("value", row[index])
-                        index += 1
         except:
             e = sys.exc_info()[0]
             logger.info("Error in reading files ::Message:: " + str(e))
-        if flag == 1:
-            indent(dataValueSet)
-            tree = ET.ElementTree(dataValueSet)
-            filename = filename[:-4]
-            xml_file = filename + ".xml"
-            tree.write(xml_file, xml_declaration=True, encoding='utf-8', method="xml")
-            #os.system("curl -d @" +xml_file+ " ""http://180.149.243.107:8080/api/dataValueSets"" -H ""Content-Type:application/xml"" -u admin:district -v")
-        else:
-            logger.info("Corrupt file, cannot be processed")
 
 
 if __name__ == '__main__':
